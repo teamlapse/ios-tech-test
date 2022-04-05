e6afed7ff70c0d565c6079c0d61ab8878350cd63
Alex little <alex@reelapp.co.uk>
2022-04-05 21:26:47 +0000

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Alex Little on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
