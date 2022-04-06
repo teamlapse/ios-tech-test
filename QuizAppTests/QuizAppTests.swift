0b8b5698ef7bff67ca5223a1859933a5996be2b6
Alex little <alex@reelapp.co.uk>
2022-04-06 19:05:32 +0000

    Update QuizAppTests.swift

--- QuizAppTests/QuizAppTests.swift
+++ QuizAppTests/QuizAppTests.swift
@@ -27,14 +27,6 @@ class QuizAppTests: XCTestCase {
         ]
     }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testDidBecomeActive() throws {
         var environment: AppEnvironment = .failing
         environment.fetchQuestions = {
